df9d0aad4d9523e975694047bd8cf5c05fb07f3f
atom <aankush>
2024-11-27 19:52:07 +0530

    added locally font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./fonts/GochiHand.ttf",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+  src: "./fonts/GochiHand.ttf",
+  variable: "--font-gochi-mono",
   weight: "100 900",
 });
 
